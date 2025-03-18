981ec0470672744e04db81dc1b3c0e8c4918841f
Rojin ORHAN <rhnn.39@gmail.com>
2025-03-18 23:07:03 +0300

    api

---
+++ ApiProjeKampi/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using ApiProjeKampi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public IActionResult ContacatList()
+        {
+            var value = _context.Contacts.ToList();
+            return Ok("kategoriler listelendi");
+        }
+    }
+}
